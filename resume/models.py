d05d8f7bbb801e1814dee87c9d632364034576cc
sudhanshu456 <spsupraj2d@gmail.com>
2020-09-08 22:25:23 +0530

    created models

--- resume/models.py
+++ resume/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+
 class ResumeDetails(models.Model):
     user= models.ForeignKey(User,on_delete=models.CASCADE)
+    profile_pic=models.ImageField()
     phone= models.IntegerField()
     address= models.TextField()
     college= models.TextField()
@@ -10,5 +13,16 @@ class ResumeDetails(models.Model):
     updated_on = models.DateTimeField(auto_now= True)
     created_on = models.DateTimeField(auto_now_add=True)
 
+class Internships(models.Model):
+    user=models.ForeignKey(ResumeDetails,on_delete=models.CASCADE)
+    Organization_name=models.TextField()
+    start=models.DateField()
+    end=models.DateField()
+    description=models.TextField()
 
-    
+class Projects(models.Model):
+    user=models.ForeignKey(ResumeDetails,on_delete=models.CASCADE)
+    project_name=models.TextField()
+    project_description=models.TextField()
+    url=models.URLField()
+    image=models.ImageField()
