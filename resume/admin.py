f6fb1db7e05b5d42cd1c6d00bc254fc1f6a56dcf
sudhanshu456 <spsupraj2d@gmail.com>
2020-09-08 22:25:14 +0530

    registered models

--- resume/admin.py
+++ resume/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Internships,ResumeDetails,Projects
 # Register your models here.
+
+admin.site.register(Internships)
+admin.site.register(ResumeDetails)
+admin.site.register(Projects)
+
